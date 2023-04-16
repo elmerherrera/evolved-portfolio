7b894854ed096831f48df7f950597ccbe3dd76a2
Elmer Herrera <dcshoeco109790@yahoo.com>
2023-04-16 14:44:28 -0400

    1

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -9,6 +9,6 @@ let age = Math.abs(agedate.getUTCFullYear() - 1970)
 
 new function aboutMe() {
     return(
-            <></>
+            <>Hello, my name is Elmer Herrera I am new to coding, but have been enjoying this new experience. I hope to learn more in the future and develop some cool things.</>
     );
 }
