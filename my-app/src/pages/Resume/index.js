ec8dc45272d66ce8aadb126a30ff4235079743fe
Elmer Herrera <dcshoeco109790@yahoo.com>
2023-04-16 20:23:03 -0400

    3

---
+++ my-app/src/pages/Resume/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Resume = () => {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <h2>Elmer Herrera</h2>
+            <p>Gainesville,GA 30504 • 123.456.7890 • dcshoeco109790@yahoo.com</p>
+            <hr />
+            <h3>Summary</h3>
+            <p>Soon to come</p>
+            </div>
+          </div>
+          </div>
+  )}
+
+
+  export default Resume
